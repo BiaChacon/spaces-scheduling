33bca91c8d1ac8ea46724280ed30a309f5100b14
brunojamelli <brunojamelli@ufrn.edu.br>
2020-09-30 15:46:46 -0300

    adding vuetify in the structure of project

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/cadastrar-espaco">Cadastrar Espaço</router-link> |
-      <router-link to="/listar-espacos">Listar Espaços</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/cadastrar-espaco">Cadastrar Espaço</router-link> |
+        <router-link to="/listar-espacos">Listar Espaços</router-link>
+      </div>
+      <router-view/>
+    
+
+  </v-app>
 </template>
 
+<script>
+// import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    // HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -17,16 +41,14 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
-  padding: 30px;
+  padding-top: 100px;
+  padding-bottom: 20px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
 }
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,10 +10,12 @@ Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vuetify from './plugins/vuetify';
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
