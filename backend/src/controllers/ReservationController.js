6192dc973677da4a7654669ece07c9f94ddd5c81
biachacon <anabheatrizchacon@outlook.com>
2020-10-11 12:44:43 -0300

    recurring reservation date validation

--- backend/src/controllers/ReservationController.js
+++ backend/src/controllers/ReservationController.js
@@ -25,12 +25,17 @@ module.exports = {
     const space = await connection('spaces').select('*').where('id', spaceId);
     if(space.length==0)
       return response.status(400).send('Bad request! Space does not exist!');
-      
+    
+    if(!normal){
+      var data1 = new Date(dateStart);
+      var data2 = new Date(dateEnd);
+      if(data1>=data2)
+        return response.status(400).send('Bad request! The end date of the recurring reservation must be after the start date!');
+    }
     //get reservations for a space
     const reservations = await connection('reservations').select('*').where('spaceId', spaceId);
     //check if the date and time are available for a given space
     const available=availabilityCheck(normal,dateStart,dateEnd,schedule,reservations);
-
     if(available){
       await connection('reservations').insert({
         id, 
@@ -54,6 +59,7 @@ module.exports = {
         spaceId 
       });
     }
+
     return response.status(400).send('Bad request!');
   },
 
