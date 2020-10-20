d8fde2438c9a05c03e52e53c617a278ea6df8cd2
brunojamelli <brunojamelli@ufrn.edu.br>
2020-10-20 16:44:06 -0300

    component menu abstraction #2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,65 +1,22 @@
 <template>
   <v-app>
-    <div>
-      <v-app-bar color="deep-purple accent-4" dense dark>
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>Spaces Scheduling</v-toolbar-title>
-        <v-spacer></v-spacer>
-      </v-app-bar>
-    </div>
-
-    <router-view />
-
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list nav dense>
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item :to="{ path: '/' }">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item :to="{ path: '/cadastrar-espaco' }">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title> Cadastrar Espaço </v-list-item-title>
-          </v-list-item>
-
-          <v-list-item :to="{ path: '/listar-espacos' }">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title> Listar Espaços </v-list-item-title>
-          </v-list-item>
-
-          <v-list-item :to="{ path: '/listar-reservas' }">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title> Listar Reservas </v-list-item-title>
-          </v-list-item>
-
-          <v-list-item :to="{ path: '/cadastro-reserva' }">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title> Reservar Sala </v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+    <app-navbar></app-navbar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
 <script>
+import AppNavbar from "./components/AppNavbar";
 export default {
   name: "App",
-  components: {},
+  components: {
+    // Footer,
+    AppNavbar,
+  },
   data: () => ({
     drawer: false,
     group: null,
---
+++ frontend/src/components/AppNavbar.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app clipped dark color="#343A40" width='180'>
+      <v-list dense>
+        <v-list-item>
+          <v-list-item-avatar>
+            <!-- <v-img src="../../public/favicon.ico"></v-img> -->
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>
+              <strong>PoP Tickets</strong>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item v-for="item in items" :key="item.title" @click="navegateTo(item.to)">
+          <!-- <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon> -->
+
+          <v-list-item-content >
+            <v-list-item-title>
+              <strong>{{ item.title }}</strong>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app color="accent-4" dense dark clipped-left>
+      <v-app-bar-nav-icon dark @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title dark>PoP Tickets</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+      <v-btn icon>
+        <v-icon>exit_to_app</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: "Home", icon: "house", to: "home" },
+        { title: "Novo Espaço", icon: "report", to: "register-space" },
+        { title: "Reservas", icon: "new_releases", to: "reservations" },
+        { title: "Faça a sua reserva", icon: "next_week", to: "register-reservation" }
+      ]
+    };
+  },
+  methods: {
+    navegateTo(where) {
+        this.$router.push({name : where});   
+    }
+  }
+};
+</script>
