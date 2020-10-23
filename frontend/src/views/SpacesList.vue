041d5196433feb158885d7e64a40def7485d5f9e
biachacon <anabheatrizchacon@outlook.com>
2020-10-23 11:46:10 -0300

    list space details

--- frontend/src/views/SpacesList.vue
+++ frontend/src/views/SpacesList.vue
@@ -6,13 +6,13 @@
   <template v-for="(space, index) in spaces">
 
     <v-card-title 
-      v-text="space.name" 
+      v-text="space.name"
       :key="space.id" 
       class="black--text pl-12 pt-12"
     >
     </v-card-title>
 
-    <v-list :key="space.id" two-line>
+    <v-list :key="space.id" disabled="true">
       <v-list-item>
         <v-list-item-icon>
           <v-icon color="indigo">
@@ -27,71 +27,69 @@
           <v-list-item-subtitle>Escola Agrícola Jundiaí</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
-      
+
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon color="indigo">
+            mdi-account-group
+          </v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title
+            v-text="space.qtdPeople"
+          ></v-list-item-title>
+          <v-list-item-subtitle>Capacidade</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon color="indigo">
+            mdi-laptop
+          </v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title
+            v-text=' space.computers ? "SIM" : "NÃO" '
+          ></v-list-item-title>
+          <v-list-item-subtitle>Computadores</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      </v-list>
+
       <v-btn
+        @click.prevent="detailSpace(space.id)"
         icon
         :key="space.id"
       >
         <v-icon color="indigo">mdi-plus</v-icon>
       </v-btn>
-
-    <v-divider 
-      v-if="index < spaces.length - 1"
-      :key="index">
-    </v-divider>
-
-<!-- 
-      <v-divider inset></v-divider>
-      <v-list-item>
-        <v-list-item-icon>
-          <v-icon color="indigo">
-            mdi-phone
-          </v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>123</v-list-item-title>
-          <v-list-item-subtitle>Ramal</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider inset></v-divider>
-
-      <v-list-item>
-        <v-list-item-icon>
-          <v-icon color="indigo">
-            mdi-account
-          </v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>nome responsavel</v-list-item-title>
-          <v-list-item-subtitle>Responsável</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item> -->
-      </v-list>
+ 
+      <v-divider 
+        v-if="index < spaces.length - 1"
+        :key="index">
+      </v-divider>
     </template>
   </v-card>
 </template>
+
 <script>
 import ApiService from '../services/ApiService';
 const http = new ApiService('spaces');
 export default {
   data: () => ({
-    spaces: [],
-    fields: [
-      { value: "id", text: "id", sortable: true },
-      { value: "name", text: "Nome", sortable: true },
-      { value: "localhost", text:"Localização", sortable: true},
-      { value: "qtdPeople", text: "Capacidade", sortable: true },
-      { value: "description", text: "Descrição" },
-    ],
+    spaces: []
   }),
   async created() {
     let response = await http.getList('spaces');
     this.spaces = response.data;
-    console.log(response.data)
   },
+  methods: {
+    async detailSpace(id) {
+      alert(id)
+    }
+  }
 };
 </script>
 <style lang="stylus" scoped></style>
