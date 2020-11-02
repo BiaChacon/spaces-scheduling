0c2183e0c269c7fac4a186171278975f9a7c0783
brunojamelli <brunojamelli@ufrn.edu.br>
2020-11-02 15:47:15 -0300

    get requisition for list all reserves from space #20

--- frontend/src/views/DetailSpace.vue
+++ frontend/src/views/DetailSpace.vue
@@ -44,13 +44,6 @@
               <strong>Responsavel: </strong>
               <p>{{ space.responsible }}</p>
             </v-card-text>
-
-            <!-- <v-card-actions>
-              <v-btn color="success">Click #1</v-btn>
-              <v-btn flat color="primary">Click #2</v-btn>
-              <v-spacer></v-spacer>
-              <v-btn icon><v-icon>bookmark</v-icon></v-btn>
-            </v-card-actions> -->
           </v-card>
         </v-flex>
       </v-layout>
--- frontend/src/views/SpaceReservations.vue
+++ frontend/src/views/SpaceReservations.vue
@@ -1,21 +1,33 @@
 <template>
   <div>
     Reservas do espaço :
-    <div v-if="space_id">
-      <strong>{{ space_id }} </strong>
-    </div>
-    
-    
+    <!-- <div v-if="space_id"> -->
+    <!-- <strong>{{ space_id }} </strong> -->
+    <li v-for="reserve in spacesReserves" :key="reserve.id">
+      <ul>{{ reserve.justification }}</ul>
+    </li>
+    <!-- </div> -->
   </div>
 </template>
 
 <script>
+// import ApiService from '../services/ApiService';
+// const http = new ApiService('space-reservations');
+import axiosConf from "../services/config";
+
 export default {
   props: ["space_id"],
-  // data: () => {
-  //   // label1: "Componente de listagem de reservas de um espaço"
-  // },
-  
+  data: () => ({
+    spacesReserves: [],
+  }),
+  async created() {
+    window.console.log(this.space_id);
+    const result = await axiosConf.get("space-reservations", {
+      params: { spaceId: this.space_id },
+    });
+    window.console.log(result.data);
+    this.reserveList = result.data;
+  },
 };
 </script>
 
