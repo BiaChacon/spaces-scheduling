a4a4dc64a5c5d2e8171fff701c85f00737ed2300
Bia Chacon <anabheatrizchacon@outlook.com>
2020-11-12 18:09:38 -0300

    space details #29

--- frontend/src/views/DetailSpace.vue
+++ frontend/src/views/DetailSpace.vue
@@ -1,35 +1,137 @@
 <template>
-  <div>
-    <h1>{{ space.name }}</h1>
-
-    <p>{{ space.description }}</p>
-
-    <p v-text=' space.special ? "Espaço Especial" : "Espaço Normal" '></p>
-
-    <p v-text=' space.computers ? "Tem Computadores" : "Não tem computadores" '></p>
-
-    <strong>Capacidade: </strong>
-    <p>{{ space.qtdPeople }}</p>
-
-    <strong>Justificativa: </strong>
-    <p>{{ space.justification }}</p>
-
-    <strong>Localização: </strong>
-    <p>{{ space.localization }}</p>
-
-    <strong>Responsavel: </strong>
-    <p>{{ space.responsible }}</p>
-
-    <!-- <v-btn
-      tile
-      dark
-      color="blue"
+  <div style="margin: 20px">
+    <v-card
+      elevation="2"
+      shaped
+      color="#fff"
+      style="margin-bottom: 50px"
     >
-      <v-icon left>
-        mdi-pencil
-      </v-icon>
-      Editar
-    </v-btn> -->
+
+      <v-card-title class="mx-5" style="font-size:3em; color:#0077c2">{{ space.name }}</v-card-title>
+
+      <v-card-text>
+      <v-list color="#fff" disabled="true">
+        <v-list-item>
+            <v-list-item-icon>
+              <v-icon color="blue">
+                mdi-home-circle-outline 
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title 
+                v-text="space.description"
+              >
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon color="blue">
+                mdi-information-outline 
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title 
+                v-text="space.justification"
+              >
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon color="blue">
+                {{space.special ? 'mdi-star-outline' : 'mdi-star-off-outline' }}
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title 
+                v-text=' space.special ? "Espaço Especial" : "Espaço Normal" '
+              >
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon color="blue">
+                mdi-account-outline
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title 
+                v-text="space.responsible"
+              >
+              </v-list-item-title>
+              <v-list-item-subtitle>Responsável</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon color="blue">
+                mdi-map-marker
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title 
+                v-text="space.localization"
+              >
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon color="blue">
+                mdi-account-group
+              </v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title
+                v-text="space.qtdPeople"
+              ></v-list-item-title>
+              <v-list-item-subtitle>Capacidade</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon color="blue">
+                mdi-laptop
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title
+                v-text=' space.computers ? "SIM" : "NÃO" '
+              ></v-list-item-title>
+              <v-list-item-subtitle>Computadores</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+         </v-list>
+        <v-card-actions>
+          <v-row
+            align="center"
+            justify="end"
+          >
+            <v-btn
+              outlined
+              rounded
+              text
+              dark
+              color="blue"
+            >
+              <v-icon left>
+                mdi-pencil
+              </v-icon>
+              Editar
+            </v-btn>
+          </v-row>
+        </v-card-actions>
+      </v-card-text>
+    </v-card>
 
     <v-row class="fill-height">
       <v-col>
