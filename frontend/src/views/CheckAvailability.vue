a5980903a66b14954b223daa9b7aeb68a15ac63a
Bia Chacon <anabheatrizchacon@outlook.com>
2020-11-13 17:55:41 -0300

    #33

--- frontend/src/views/CheckAvailability.vue
+++ frontend/src/views/CheckAvailability.vue
@@ -81,62 +81,64 @@
               <v-card-title
                 v-text="space.name"
                 :key="space.id"
-                class="black--text pl-12 pt-12"
+                disabled="true"
+                class="mx-5"
+                style="font-size: 2em; color: #0077c2"
               >
               </v-card-title>
+              <v-card-text :key="space.id">
+                <v-list :key="space.id" disabled="true">
+                  <v-list-item>
+                    <v-list-item-icon>
+                      <v-icon color="blue"> mdi-map-marker </v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title v-text="space.localization">
+                      </v-list-item-title>
+                      <v-list-item-subtitle>Localização</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
 
-              <v-list :key="space.id" disabled="true">
-                <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon color="indigo"> mdi-map-marker </v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-content>
-                    <v-list-item-title v-text="space.localization">
-                    </v-list-item-title>
-                    <v-list-item-subtitle
-                      >Escola Agrícola Jundiaí</v-list-item-subtitle
-                    >
-                  </v-list-item-content>
-                </v-list-item>
+                  <v-list-item>
+                    <v-list-item-icon>
+                      <v-icon color="blue"> mdi-account-group </v-icon>
+                    </v-list-item-icon>
 
-                <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon color="indigo"> mdi-account-group </v-icon>
-                  </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title
+                        v-text="space.qtdPeople"
+                      ></v-list-item-title>
+                      <v-list-item-subtitle>Capacidade</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
 
-                  <v-list-item-content>
-                    <v-list-item-title
-                      v-text="space.qtdPeople"
-                    ></v-list-item-title>
-                    <v-list-item-subtitle>Capacidade</v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-
-                <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon color="indigo"> mdi-laptop </v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-content>
-                    <v-list-item-title
-                      v-text="space.computers ? 'SIM' : 'NÃO'"
-                    ></v-list-item-title>
-                    <v-list-item-subtitle>Computadores</v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list>
-
-              <v-row align="center" justify="space-around" :key="space.id">
-                <v-btn
-                  @click="sendToDetail('detail-space', space)"
-                  :key="space.id"
-                  tile
-                  dark
-                  color="indigo"
-                >
-                  <v-icon left>mdi-plus</v-icon>
-                  Detalhes
-                </v-btn>
-              </v-row>
+                  <v-list-item>
+                    <v-list-item-icon>
+                      <v-icon color="blue"> mdi-laptop </v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title
+                        v-text="space.computers ? 'SIM' : 'NÃO'"
+                      ></v-list-item-title>
+                      <v-list-item-subtitle>Computadores</v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list>
+              </v-card-text>
+              <v-card-actions :key="space.id">
+                <v-row align="center" justify="space-around">
+                  <v-btn
+                    @click="sendToDetail('detail-space', space)"
+                    rounded
+                    small
+                    dark
+                    color="blue"
+                  >
+                    <v-icon left>mdi-plus</v-icon>
+                    Detalhes
+                  </v-btn>
+                </v-row>
+              </v-card-actions>
               <v-divider v-if="index < spaces.length - 1" :key="index">
               </v-divider>
             </template>
