aca3c091e6e6f250e54cdceb393ade41c64379bf
Bia Chacon <anabheatrizchacon@outlook.com>
2020-11-18 11:23:15 -0300

    #34

--- frontend/src/components/AppNavbar.vue
+++ frontend/src/components/AppNavbar.vue
@@ -1,35 +1,33 @@
 <template>
   <div>
-      <v-app-bar 
-        app 
-        clipped-rigth 
-        flat 
-        color="#0091EA" 
+    <v-app-bar app clipped-rigth flat color="#0091EA" dark>
+      <v-app-bar-nav-icon
         dark
-      >
-      <v-app-bar-nav-icon dark @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title dark class="text-center">{{$store.getters.titlePage}}</v-toolbar-title>
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title dark class="text-center">{{
+        $store.getters.titlePage
+      }}</v-toolbar-title>
       <div class="flex-grow-1"></div>
       <v-btn icon>
-        <!-- <v-icon>exit_to_app</v-icon> -->
+        <v-icon>mdi-exit-to-app</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-navigation-drawer 
+    <v-navigation-drawer
       persistent
       v-model="drawer"
       enable-resize-watcher
       fixed
       app
-      color="#0091EA" 
+      color="#0091EA"
       dark
       width="270"
     >
-      <v-list dense>
+      <v-list nav dense>
         <v-list-item>
           <!-- <v-list-item-avatar>
             <v-img src="../../public/favicon.ico"></v-img>
           </v-list-item-avatar> -->
-
           <v-list-item-content>
             <v-list-item-title>
               <strong>Agendamento de Salas</strong>
@@ -37,17 +35,44 @@
           </v-list-item-content>
         </v-list-item>
         <v-divider></v-divider>
-        <v-list-item v-for="item in items" :key="item.title" @click="navegateTo(item.to, item.page)">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
 
-          <v-list-item-content >
-            <v-list-item-title>
-              <strong>{{ item.title }}</strong>
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+        <div v-for="(item, i) in items" :key="i">
+          <v-list-item
+            v-if="!item.subLinks"
+            @click="navegateTo(item.to)"
+            :active-class="color"
+            avatar
+            class="v-list-item"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item>
+
+          <v-list-group
+            v-else
+            :key="item.title"
+            no-action
+            :prepend-icon="item.icon"
+            :value="false"
+          >
+            <template v-slot:activator>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </template>
+
+            <v-list-item
+              v-for="sublink in item.subLinks"
+              @click="navegateTo(sublink.to)"
+              :key="sublink.text"
+            >
+              <v-list-item-icon>
+                <v-icon>{{ sublink.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>{{ sublink.text }}</v-list-item-title>
+            </v-list-item>
+          </v-list-group>
+        </div>
       </v-list>
     </v-navigation-drawer>
   </div>
@@ -59,20 +84,45 @@ export default {
     return {
       drawer: true,
       items: [
-        { title: "Espaços", page:"Espaços", icon: "mdi-home-city-outline", to: "home" },
-        { title: "Ver disponibilidade", page:"Ver disponibilidade", icon: "mdi-calendar-search ", to: "check-availability" },
-        { title: "Cadastrar Reserva Normal", page:"Cadastro de Reserva Normal", icon: "mdi-calendar-plus", to: "register-reservation-normal" },
-        { title: "Cadastrar Reserva Recorrente", page:"Cadastro de Reserva Recorrente", icon: "mdi-calendar-plus", to: "register-reservation-recurring" },
-        { title: "Cadastrar Espaço", page:"Cadastro de Espaço", icon: "mdi-home-plus", to: "register-space" },
+        {
+          title: "Espaços",
+          icon: "mdi-home-city-outline",
+          to: "home",
+        },
+        {
+          title: "Ver disponibilidade",
+          icon: "mdi-calendar-search ",
+          to: "check-availability",
+        },
+        {
+          title: "Cadastrar Reserva",
+          icon: "mdi-calendar-plus",
+          subLinks: [
+            {
+              to: "register-reservation-recurring",
+              text: "Recorrente",
+              icon: "mdi-calendar-week",
+            },
+            {
+              to: "register-reservation-normal",
+              text: "Normal",
+              icon: "mdi-calendar-today",
+            },
+          ],
+        },
+        {
+          title: "Cadastrar Espaço",
+          icon: "mdi-home-plus",
+          to: "register-space",
+        },
       ],
       namePage: "Espaços",
     };
   },
   methods: {
-    navegateTo(where, name) {
-      this.namePage = name;
-      this.$router.push({name : where});
-    }
-  }
+    navegateTo(where) {
+      this.$router.push({ name: where });
+    },
+  },
 };
 </script>
