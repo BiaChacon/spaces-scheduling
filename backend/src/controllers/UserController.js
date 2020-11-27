12fbe9aa770510780c77d0cd60049c762585ec8d
Bia Chacon <anabheatrizchacon@outlook.com>
2020-11-27 13:21:51 -0300

    user: model and controller

---
+++ backend/src/controllers/UserController.js
@@ -0,0 +1,64 @@
+const generateUniqueId = require('../utils/generateUniqueId');
+const connection = require('../database/connection');
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+  async login(request, response){
+    //esse teste abaixo deve ser feito no seu banco de dados
+    if(request.body.username === 'luiz' && request.body.password === '12345'){
+      //auth ok
+      const id = 1; //esse id viria do banco de dados
+      const token = jwt.sign({ id }, process.env.SECRET, {
+        expiresIn: 300 // expires in 5min
+      });
+      return response.json({ auth: true, token: token });
+    }
+
+    return response.status(500).json({message: 'Login inválido!'});
+  },
+
+  async logout(request, response){
+    request.user.deleteToken(req.token,(err,user)=>{
+      if(err)
+        return res.status(400).send(err);
+
+      return response.sendStatus(200);
+    });
+  },
+
+  async profile(request, response) {
+    return response.json({
+      isAuth: true,
+      id: request.user.id,
+      username: request.user.username,
+    });
+  },
+
+  async index(request, response) {
+    const users = await connection('users').select('*');
+
+    return response.json(users);
+  },
+
+  async create(request, response){
+    const {
+      admin,
+      username,
+      password,
+      password2,
+    } = request.body;
+
+    const id = generateUniqueId();
+
+    await connection('users').insert({
+      id,
+      admin,
+      username,
+      password,
+      password2,
+      token
+    });
+    return response.json({ id });
+  },
+
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const { celebrate, Segments, Joi } = require('celebrate');
 require("dotenv-safe").config();
-const jwt = require('jsonwebtoken');
-// const verifyJWT = require('../utils/auth');
+// const jwt = require('jsonwebtoken');
+const verifyJWT = require('../utils/auth');
 
 const SpaceController = require('./controllers/SpaceController');
 const ReservationController = require('./controllers/ReservationController');
+const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
+
+routes.post('/login', UserController.login);
+routes.get('/logout', verifyJWT, UserController.logout);
+routes.get('/profile', verifyJWT, UserController.profile);
+routes.post('/register-user', UserController.create)
 
 routes.get('/spaces', SpaceController.index);
 routes.get('/spaces/:id', SpaceController.show);
@@ -45,20 +51,5 @@ routes.post('/register-reservation', celebrate({
         spaceId: Joi.string().required()
     })
 }), ReservationController.create);
-
-
-routes.post('/login', (req, res, next) => {
-    //esse teste abaixo deve ser feito no seu banco de dados
-    if(req.body.user === 'luiz' && req.body.password === '12345'){
-      //auth ok
-      const id = 1; //esse id viria do banco de dados
-      const token = jwt.sign({ id }, process.env.SECRET, {
-        expiresIn: 300 // expires in 5min
-      });
-      return res.json({ auth: true, token: token });
-    }
-    
-    res.status(500).json({message: 'Login inválido!'});
-})
 
 module.exports = routes;
