6f353e7ede0d5eaede9d4c20e0f5e37f883433f3
Bia Chacon <anabheatrizchacon@outlook.com>
2020-12-12 18:11:00 -0300

    url

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const http = axios.create({
-  baseURL: 'http://localhost:3333/',
+  baseURL: 'https://spaces-scheduling.herokuapp.com/',
 });
 
 export default http;
