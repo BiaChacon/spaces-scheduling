6ee251cbf067d295c64dc927d6c568afac36b12b
brunojamelli <brunojamelli@ufrn.edu.br>
2020-12-13 10:12:44 -0300

    change in base api url config

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const http = axios.create({
-  baseURL: 'http://localhost:3333/',
+  baseURL: "https://spaces-scheduling.herokuapp.com/",
+  // baseURL: "http://localhost:3333/"
 });
 
 export default http;
